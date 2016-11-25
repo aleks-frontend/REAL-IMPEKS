a4da6baec6a2403c72f3667b86ff94e4fcbc43d3
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2016-11-25 12:02:49 +0100

    Added all the files

---
+++ js/mobile.js
@@ -0,0 +1,66 @@
+jQuery(document).ready(function(){
+ //<b class="caret"></b>
+
+ var menu_title = "Meni";
+ var scr_width = 965;
+
+ //show the submenu when one of submenu item is active? 1 is true, 0 is false
+ var show_active_submenu = 1;
+
+
+ var vsmenucart = jQuery('.mobile-cart');
+ var vsmenu = jQuery('#main-menu');
+ vsmenu.find('li > ul').css('display', 'none');
+ vsmenu.find('li > ul').parent().append("<b class='caret'></b>");
+ //vsmenu.find('li > ul > li > ul').parent().append("<b class='caret caret_submenu'></b>");
+
+ vsmenu.find('li > ul').closest('li').children('a').bind('click', function(e){
+  e.preventDefault();
+  jQuery(this).closest('li').children('ul').stop().slideToggle('fast');
+  jQuery(this).closest('li').siblings().find('ul').slideUp('fast');
+ });
+
+ if(show_active_submenu == 1){
+  vsmenu.find('li.active').parents("ul").show();
+ }
+ 
+
+ 
+ //<li class="menu_head"><a href="#">Menu</a></li>
+ vsmenucart.append('<div class="menu_head"><a href="#">'+ menu_title +'</a></div>');
+ vsmenucart.find('.menu_head').append("");
+ vsmenucart.find('.menu_head').css('display', 'none');
+
+ if(jQuery(window).width() < scr_width){
+  vsmenu.find('li').css('display', 'none');
+  vsmenucart.find('.menu_head').css('display', 'inline-block');
+ }
+
+ jQuery(window).resize(function() {
+
+   if(jQuery(window).width() < scr_width){
+     
+     vsmenu.find('li').css('display', 'none');
+     vsmenucart.find('.menu_head').css('display', 'inline-block');
+   }else{
+     
+     vsmenu.find('li').css('display', 'inline-block');
+     vsmenucart.find('.menu_head').css('display', 'none');
+   } 
+
+ });
+
+ var open = 0;
+ vsmenucart.find('.menu_head > a').bind('click',function(e){
+  e.preventDefault();
+  if(open == 0){ 
+   vsmenu.find('li').css('display', 'block');
+   open = 1;
+  }else{
+   vsmenu.find('li').not('.menu_head').css('display', 'none');
+   open = 0;
+  }
+ });
+
+ 
+});
---
+++ o-nama.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">
+<!-- saved from url=(0030)http://www.realimpeks.co.rs/ -->
+<html lang="sr" dir="ltr" version="HTML+RDFa 1.1" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/terms/" xmlns:foaf="http://xmlns.com/foaf/0.1/" xmlns:og="http://ogp.me/ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#" xmlns:sioc="http://rdfs.org/sioc/ns#" xmlns:sioct="http://rdfs.org/sioc/types#" xmlns:skos="http://www.w3.org/2004/02/skos/core#" xmlns:xsd="http://www.w3.org/2001/XMLSchema#" class="js">
+  <head profile="http://www.w3.org/1999/xhtml/vocab">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+    <meta content=" Real Impeks - servisiranje i popravku pumpi za vodu" about="/real-impeks-servisiranje-i-popravku-pumpi-za-vodu" property="dc:title">
+    <link rel="shortcut icon" href="http://www.realimpeks.co.rs/favicon.ico" type="image/vnd.microsoft.icon">
+    <meta name="description" content="Preduzeće “Real Impeks” d.o.o. - Pumpe za vodu - Servis i prodaja pumpi i elektromotora - Premotavanje elektromotora - Busenje bunara - Opremanje bunara - Navodnjavanje - Izrada upravljačkih ormana za pumpe i pumpne sisteme - Fabrika vode">
+    <meta name="abstract" content="Real Impeks d.o.o. - Pumpe za vodu - Servis pumpi - Premotavanje elektromotora - Fabrika vode">
+    <meta name="keywords" content="pumpe, pumpe za vodu, elektromotori, premotavanje, viklovanje, servis pumpi, busenje bunara, opremanje bunara, navodnjavanje, fabrika vode, wilo, grundfos, pedrollo, sever">
+    <meta name="generator" content="RI-generator">
+    <link rel="canonical" href="http://www.realimpeks.co.rs/">
+    <link rel="shortlink" href="http://www.realimpeks.co.rs/">
+    <title>O nama :: Real Impeks</title>
+    <style type="text/css" media="all">@import url("/css/system.base.css");
+      @import url("/css/system.menus.css");
+      @import url("/css/system.messages.css");
+      @import url("/css/system.theme.css");
+    </style>
+    <style type="text/css" media="all">@import url("/css/alpha-reset.css");
+      @import url("/css/alpha-mobile.css");
+      @import url("/css/alpha-alpha.css");
+      @import url("/css/formalize.css");
+      @import url("/css/omega-text.css");
+      @import url("/css/omega-branding.css");
+      @import url("/css/omega-menu.css");
+      @import url("/css/omega-forms.css");
+      @import url("/css/omega-visuals.css");
+      @import url("/css/global.css");
+    </style>
+    <!--[if (lt IE 9)&(!IEMobile)]>
+    <style type="text/css" media="all">@import url("/css/realimpeks-alpha-default.css");
+      @import url("/css/realimpeks-alpha-default-normal.css");
+      @import url("/css/alpha-default-normal-24.css");
+    </style>
+    <![endif]-->
+    <!--[if gte IE 9]><!-->
+    <style type="text/css" media="all and (min-width: 740px) and (min-device-width: 740px), (max-device-width: 800px) and (min-width: 740px) and (orientation:landscape)">
+      @import url("/css/realimpeks-alpha-default.css");
+      @import url("/css/realimpeks-alpha-default-narrow.css");
+      @import url("/css/alpha-default-narrow-24.css");
+    </style>
+    <!--<![endif]-->
+    <!--[if gte IE 9]><!-->
+    <style type="text/css" media="all and (min-width: 980px) and (min-device-width: 980px), all and (max-device-width: 1024px) and (min-width: 1024px) and (orientation:landscape)">
+      @import url("/css/realimpeks-alpha-default.css");
+      @import url("/css/realimpeks-alpha-default-normal.css");
+      @import url("/css/alpha-default-normal-24.css");
+    </style>
+    <!--<![endif]-->
+    <!--[if gte IE 9]><!-->
+    <style type="text/css" media="all and (min-width: 1220px)">
+      @import url("/css/realimpeks-alpha-default.css");
+      @import url("/css/realimpeks-alpha-default-wide.css");
+      @import url("/css/alpha-default-wide-24.css");
+    </style>
+    <!--<![endif]-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-6807109-20', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <script async="" src="/js/analytics.js"></script><script type="text/javascript" src="/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.once.js"></script>
+    <script type="text/javascript" src="/js/jquery.colorbox-min.js"></script>
+    <script type="text/javascript" src="/js/mobile.js"></script>
+    <script type="text/javascript" src="/js/jquery.formalize.js"></script>
+  </head>
+  <body class="html front not-logged-in page-node page-node- page-node-29 node-type-page omega-mediaqueries-processed responsive-layout-wide">
+    <div id="omega-media-query-dummy">
+      <style media="all">#omega-media-query-dummy { position: relative; z-index: -1; }</style>
+      <!--[if (lt IE 9)&(!IEMobile)]>
+      <style media="all">#omega-media-query-dummy { z-index: 1; }</style>
+      <![endif]-->
+      <style media="all and (min-width: 740px) and (min-device-width: 740px), (max-device-width: 800px) and (min-width: 740px) and (orientation:landscape)">#omega-media-query-dummy { z-index: 0; }</style>
+      <style media="all and (min-width: 980px) and (min-device-width: 980px), all and (max-device-width: 1024px) and (min-width: 1024px) and (orientation:landscape)">#omega-media-query-dummy { z-index: 1; }</style>
+      <style media="all and (min-width: 1220px)">#omega-media-query-dummy { z-index: 2; }</style>
+    </div>
+    <div id="skip-link">
+      <a href="http://www.realimpeks.co.rs/" class="element-invisible element-focusable">Skip to main content</a>
+    </div>
+    <div class="page clearfix" id="page">
+      <header id="section-header" class="section section-header">
+        <div id="zone-branding-wrapper" class="zone-wrapper zone-branding-wrapper clearfix">
+          <div id="zone-branding" class="zone zone-branding clearfix container-24">
+            <div class="grid-6 suffix-3 region region-branding" id="region-branding">
+              <div class="region-inner region-branding-inner">
+                <div class="branding-data clearfix">
+                  <div class="logo-img">
+                    <a href="http://www.realimpeks.co.rs/" rel="home" title="" class="active"><img src="/img/logo.png" alt="" id="logo"></a>
+                  </div>
+                </div>
+                <div class="block block-block block-4 block-block-4 odd block-without-title" id="block-block-4">
+                  <div class="block-inner clearfix">
+                    <div class="content clearfix">
+                      <p>pumpe za vodu - oprema - inženjering</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="grid-15 region region-menu" id="region-menu">
+              <div class="region-inner region-menu-inner">
+                <div class="mobile-cart">
+                  <div class="menu_head" style="display: none;">
+                    <a href="http://www.realimpeks.co.rs/#">Meni</a>
+                  </div>
+                </div>
+                <nav class="navigation">
+                  <h2 class="element-invisible">Main menu</h2>
+                  <ul id="main-menu" class="links inline clearfix main-menu">
+                    <li class="menu-218 first active"><a href="http://www.realimpeks.co.rs/" title="" class="active">Početna</a></li>
+                    <li class="menu-576"><a href="http://www.realimpeks.co.rs/o-nama.php">O nama</a></li>
+                    <li class="menu-557"><a href="http://www.realimpeks.co.rs/nas-tim.php">Naš tim</a></li>
+                    <li class="menu-558"><a href="http://www.realimpeks.co.rs/reference.php">Reference</a></li>
+                    <li class="menu-738"><a href="http://www.realimpeks.co.rs/download.php" title="Download">Download</a></li>
+                    <li class="menu-551"><a href="http://www.realimpeks.co.rs/kontakt.php">Kontakt</a></li>
+                    <li class="menu-739 last"><a href="http://www.realimpeks.co.rs/posao.php">Posao</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section id="section-content" class="section section-content">
+        <div id="zone-content-wrapper" class="zone-wrapper zone-content-wrapper clearfix">
+          <div id="zone-content" class="zone zone-content clearfix container-24">
+            <aside class="grid-8 region region-sidebar-first" id="region-sidebar-first">
+              <div class="region-inner region-sidebar-first-inner">
+                <section class="block block-block block-8 block-block-8 odd" id="block-block-8">
+                  <div class="block-inner clearfix">
+                    <h2 class="block-title">Izdvajamo</h2>
+                    <div class="content clearfix">
+                      <ul>
+                        <li>Bušenje bunara - Opremanje bunara - Ključ u ruke</li>
+                        <li>Održavanje sistema u zgradama / višespratnicama</li>
+                        <li>Dekorativna fontana na Srebrnom jezeru</li>
+                      </ul>
+                      <hr>
+                      <p>&nbsp;</p>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </aside>
+            <div class="grid-15 prefix-1 region region-content" id="region-content">
+              <div class="region-inner region-content-inner">
+                <a id="main-content"></a>
+                <section class="block block-block block-7 block-block-7 odd" id="block-block-7">
+                  <div class="block-inner clearfix">
+                    <h2 class="block-title">O nama</h2>
+                    <div class="content clearfix">
+                      <p>Preduzeće “Real Impeks” d.o.o. datira još od 1991. godine. U početku se bavilo sastavljanjem i ožičavanjem upravljačkih ormana za pumpne sisteme, nekadašnjeg giganta u ovoj oblasti, preduzeća “Sever”, Subotica što je dovelo do širenja interesovanja prema samim pumpnim sistemi i pumpama za vodu.</p>
+                      <p>Nakon nekoliko godina, osnovna delatnost prelazi u servisiranje i popravku pumpi za vodu. Interesovanja su se širila iz godine u godinu kako bi danas preduzeće “Real Impeks” postalo lider u oblasti premotavanja dubinskih potapajućih elektromotora na teritoriji Srbije.</p>
+                      <p>Naša vizija je da budemo sinonim za preduzeće koje stalno postavlja više standarde kako u oblasti poslovanja, pružanja usluge i trgovine, tako i u oblasti življenja i koje, ne samo da uvodi nove navike i trendove, nego ih i predviđa i kreira. Da budemo sinonim za preduzeće koje posluje u dosluhu s vremenom koje dolazi.</p>
+                      <p>“Real Impeks” d.o.o. je preduzeće u razvoju koje teži zadovoljenju potreba vlasnika, unapređenju svojih procesa rada, zadovoljenju korisnika usluga i obezbeđenja kvaliteta života i uslova društvenih potreba.</p>
+                      <p>
+                        <a href="/download/obrazac_za_identifikaciju.pdf" target="_blank">
+                          <img alt="" src="/img/pdf_icon.png" style="height:50px; width:50px" />
+                        </a>
+                        Identifikacija
+                        <a href="/download/obrazac_pdv.pdf" target="_blank">
+                          <img alt="" src="/img/pdf_icon.png" style="height:50px; width:50px" />
+                        </a>
+                        PDV obrazac
+                      </p>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer id="section-footer" class="section section-footer">
+        <div id="zone-footer-wrapper" class="zone-wrapper zone-footer-wrapper clearfix">
+          <div id="zone-footer" class="zone zone-footer clearfix container-24">
+            <div class="grid-7 region region-footer-first" id="region-footer-first">
+              <div class="region-inner region-footer-first-inner">
+                <section class="block block-block block-1 block-block-1 odd" id="block-block-1">
+                  <div class="block-inner clearfix">
+                    <h2 class="block-title">Real Impeks d.o.o.</h2>
+                    <div class="content clearfix">
+                      <p>
+                        <img alt="" src="/img/footer_info1.png" style="height:20px; width:13px">&nbsp;&nbsp;&nbsp; Marka Oreškovića 41c, 24413 Palić, Srbija
+                      </p>
+                      <p>
+                        <img alt="" src="/img/footer_info2.png" style="height:20px; width:11px">&nbsp;&nbsp;&nbsp; +381 (0) 24 602 675&nbsp; +381 (0) 24 604 150
+                      </p>
+                      <p>
+                        <img alt="" src="/img/footer_info3.png" style="height:14px; width:20px">&nbsp; <a href="mailto:kontakt@realimpeks.co.rs">kontakt@realimpeks.co.rs</a>
+                      </p>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+            <div class="grid-6 region region-footer-second" id="region-footer-second">
+              <div class="region-inner region-footer-second-inner">
+                <section class="block block-block block-2 block-block-2 odd" id="block-block-2">
+                  <div class="block-inner clearfix">
+                    <h2 class="block-title">Pravni podaci</h2>
+                    <div class="content clearfix">
+                      <p><strong>PIB:</strong> 100844331<br><strong>Marični broj:</strong> 08734267<br><strong>Raiffeisen Bank:</strong> 265-2410310000285-16<br><strong>Unicredit Bank:</strong> 170-5176-06</p>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+            <div class="grid-11 region region-footer-third" id="region-footer-third">
+              <div class="region-inner region-footer-third-inner">
+                <div class="block block-system block-menu block-main-menu block-system-main-menu odd block-without-title" id="block-system-main-menu">
+                  <div class="block-inner clearfix">
+                    <div class="content clearfix">
+                      <ul class="menu">
+                        <li class="first leaf"><a href="http://www.realimpeks.co.rs/" title="" class="active">Početna</a></li>
+                        <li class="leaf"><a href="http://www.realimpeks.co.rs/o-nama.php">O nama</a></li>
+                        <li class="leaf"><a href="http://www.realimpeks.co.rs/nas-tim.php">Naš tim</a></li>
+                        <li class="leaf"><a href="http://www.realimpeks.co.rs/reference.php">Reference</a></li>
+                        <li class="leaf"><a href="http://www.realimpeks.co.rs/download.php" title="Download">Download</a></li>
+                        <li class="leaf"><a href="http://www.realimpeks.co.rs/kontakt.php">Kontakt</a></li>
+                        <li class="last leaf"><a href="http://www.realimpeks.co.rs/posao.php">Posao</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="block block-block block-3 block-block-3 even block-without-title" id="block-block-3">
+                  <div class="block-inner clearfix">
+                    <div class="content clearfix">
+                      <p>Real Impeks d.o.o. 2014 - Sajt izradio <a href="http://www.webdizajn.co.rs/" target="_blank">Studio Present</a><br>
+                        Dizajn i vizuelni identitet - Danilo Svilar 064 1991 132
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
